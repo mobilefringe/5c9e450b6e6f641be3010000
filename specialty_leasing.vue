2b93ef64615fdb72d7e3c05721e6658298ab3beb
alex@mobilefringe.com @ CodeCloud <alex@mobilefringe.com @ CodeCloud>
2019-04-03 10:49:08 -0400

    Prairie Mall 2.0 1.0  /specialty_leasing.vue

--- specialty_leasing.vue
+++ specialty_leasing.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-if="currentPage">
-        <div v-if="pageBanner" class="page_header" v-bind:style="{ backgroundImage: 'url('http://via.placeholder.com/1920x300')' }">
+        <div v-if="pageBanner" class="page_header" style="{ backgroundImage: url('http://via.placeholder.com/1920x300') }">
 			<div class="site_container">
 				<div class="header_content">
 					<!--<h1 v-if="locale=='en-ca'">{{currentPage.title}}</h1>-->
