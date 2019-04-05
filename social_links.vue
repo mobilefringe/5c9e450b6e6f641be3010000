d7648dafdafe9ae54e3b66ee8e251ced2a048169
alex@mobilefringe.com @ CodeCloud <alex@mobilefringe.com @ CodeCloud>
2019-04-05 11:28:20 -0400

    Prairie Mall 2.0 1.0  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -2,6 +2,7 @@
     <div>
         <a href="https://www.facebook.com/Prairiemall" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i><p style="display:none">Facebook</p></a>
         <a href="https://www.twitter.com/prairiemall" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i><p style="display:none">Twitter</p></a>
+        <a href="https://www.pinterest.ca/prairiemall" target="_blank"><i class="fa fa-pinterest" aria-hidden="true"></i><p style="display:none">Twitter</p></a>
         <a href="https://www.instagram.com/prairiemall/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i><p style="display:none">Instagram</p></a>
     </div>
     
