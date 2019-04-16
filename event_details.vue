6b385bc06cb60907266117626a0992e2fd7a47c5
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-16 12:46:46 -0400

    Prairie Mall 2.0 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -32,6 +32,7 @@
 					</social-sharing>
 					<div class="col-sm-12 no_padding text-center">
 						<img v-if="!_.includes(currentEvent.image_url, 'missing')" v-lazy="currentEvent.image_url" class="image" :alt="currentEvent.name"/>
+						<img v-else class="image" v-lazy="currentEvent.store.store_front_url_abs" />
 						<div class="text-left promo_description">
 							<p v-if="locale=='en-ca'" v-html="currentEvent.rich_description"></p>
 							<p v-else v-html="currentEvent.rich_description_2"></p>
