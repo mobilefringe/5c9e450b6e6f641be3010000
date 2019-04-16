a9e78a6c29de499d3430adab06e1f816af86c53f
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-16 15:00:19 -0400

    Prairie Mall 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -250,6 +250,16 @@
                         this.$router.replace('/');
                     }
                 },
+                isMultiDay(promo) {
+                    var timezone = this.timezone
+                    var start_date = moment(promo.start_date).tz(timezone).format("MM-DD-YYYY")
+                    var end_date = moment(promo.end_date).tz(timezone).format("MM-DD-YYYY")
+                    if (start_date === end_date) {
+                        return false
+                    } else {
+                        return true
+                    }
+                },
                 dropPin(store) {
                     this.$refs.mapplic_ref.showLocation(store.svgmap_region);
                 },
