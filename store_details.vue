09e2942735d12028d72c545757559d0f182092fb
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-17 15:37:34 -0400

    Prairie Mall 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -66,7 +66,12 @@
 						<div class="row store_promo_dets text-left" >
 							<div class="col-sm-6 margin_30" v-for="promo in promotions">
 								<div class="promo_div_image text-center" v-lazy:background-image="checkImageURL(promo)">
-									<!--<img v-lazy="checkImageURL(promo)" class="image" alt=""/>-->
+									<div v-if="!checkImageURL(promo)" class="store_details_image center-block">
+                                    <div class="no_logo">
+                                        <img class="store_img" src="//www.mallmaverick.com/system/site_images/photos/000/041/782/original/transparent_logo.png?1533845225" alt="">
+                                        <h2 class="store_details_name">{{ promo.store.name }}</h2>
+                                    </div>    
+                                </div>
 								</div>
 								<div class="store_promo_dets_container padding_tb_20">
 								    <p class="promo_div_name" v-if="locale=='fr-ca'">{{promo.name_2}}</p>
