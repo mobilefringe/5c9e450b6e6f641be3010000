91f1cb6cd6bd3e3973375f0a0a7aab104fa67e00
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-17 12:16:29 -0400

    Prairie Mall 2.0 1.0  /search_results.vue

--- search_results.vue
+++ search_results.vue
@@ -17,7 +17,7 @@
                         <div class="row result_container_row">
                            <div v-if="result.is_store" class="col-sm-3 store_details_image center-block">
                                 <div v-if="result.hasOwnProperty('promo_image_url_abs')">
-                                    <img v-else-if="checkPromoImage(result)" :src="result.promo_image_url_abs" />
+                                    <img v-else-if="checkPromoImage(result)" :src="result.store_image_url_abs" />
                                     <img v-if="checkPromoImage(result)" :src="result.store.image_url" />
                                     <div v-else class="no_logo">
                                         <img src="//codecloud.cdn.speedyrails.net/sites/5b88438d6e6f641e8d3c0000/image/png/1536092029690/transparent_logo.png">
