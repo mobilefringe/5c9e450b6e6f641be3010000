a24465c62a8ea0f645646299eb090ec3ec065ac9
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-17 14:47:13 -0400

    Prairie Mall 2.0 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -194,11 +194,11 @@
                                 'length': 150
                             });
                             if (value.store != null && value.store != undefined && _.includes(value.store.store_front_url_abs, 'missing')) {
-                                value.store.store_front_url_abs = vm.property.default_logo_url;
+                                value.store.store_front_url_abs = vm.property.default_logo_url_black;
                             }
                             else if (value.store == null || value.store == undefined) {
                                 value.store = {};
-                                value.store.store_front_url_abs =  vm.property.default_logo_url;
+                                value.store.store_front_url_abs =  vm.property.default_logo_url_black;
                             }
                             temp_promo.push(value);
                         }
