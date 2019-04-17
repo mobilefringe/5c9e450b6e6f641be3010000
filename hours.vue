02b4466b3cb9fd9600e7b58ed14ec16268f81325
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-17 13:22:04 -0400

    Prairie Mall 2.0 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -15,8 +15,8 @@
                         <h3 class="hours_heading text-left" >{{$t("hours_page.reg_hours")}}</h3>
                         <div id="hours_container" class="hours_container">
                             <div class="hours_div" v-for="hour in hours">
-                                <p>{{hour.day_of_week | moment("dddd", timezone)}} : </p>
-                                <p>{{hour.open_time | moment("h:mm A", timezone)}} - {{hour.close_time | moment("h:mm A", timezone)}}</p>
+                                <p class="hours_days">{{hour.day_of_week | moment("dddd", timezone)}} : </p>
+                                <p class="hours_days">{{hour.open_time | moment("h:mm A", timezone)}} - {{hour.close_time | moment("h:mm A", timezone)}}</p>
                             </div>
                         </div>
                     </div>
