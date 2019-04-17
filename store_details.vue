26bae375eb74c61041e096d7adfb07b63b0f86d6
jeff@mobilefringe.com @ CodeCloud <jeff@mobilefringe.com @ CodeCloud>
2019-04-17 15:24:39 -0400

    Prairie Mall 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -91,7 +91,7 @@
 								    <p class="promo_div_name" v-if="locale=='fr-ca'">{{job.name_2}}</p>
 								    <p class="promo_div_name" v-else>{{job.name}}</p>
     								<p class="promo_div_date" v-if="isMultiDay(job)"><i class="fa fa-calendar"></i>{{job.start_date | moment("MMM D", timezone)}} - {{job.end_date | moment("MMM D", timezone)}}</p>
-							        <p class="promo_div_date" v-else><i class="fa fa-calendar"></i>{{ jobstart_date | moment("MMM D", timezone) }}</p>
+							        <p class="promo_div_date" v-else><i class="fa fa-calendar"></i>{{ job.start_date | moment("MMM D", timezone) }}</p>
     								<div>
     								    <button class="contest_btn">
         									<router-link :to="'/careers/'+ job.slug" class="" >{{$t("jobs_page.read_more")}}</router-link>
