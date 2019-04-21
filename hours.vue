e67ddbdbbd4813862d582ca844a61d75c46566bb
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-21 18:34:12 -0400

    Prairie Mall 2.0 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -37,7 +37,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="padding_top_40"></div>
+                <div class="margin_40"></div>
             </div>
         </div >
     </div>
