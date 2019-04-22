f775604270b7a292b78ec5b5d34b513d1e20fec6
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-21 22:25:59 -0400

    Prairie Mall 2.0 1.0  /main.js

--- main.js
+++ main.js
@@ -157,10 +157,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
         methods: {
             loadData: async function() {
                 try {
-                    await this.$store.dispatch('initializeApi', {
-                        site: "prairiemall",
-                        version: "v4"
-                    });
+                    await this.$store.dispatch('initializeApi', { site: "prairiemall", version: "v4" });
                     await Promise.all([this.$store.dispatch("getData", "property")]);
                     this.property.mm_host = this.property.mm_host.replace("http:", "");
                     let results = await Promise.all([
