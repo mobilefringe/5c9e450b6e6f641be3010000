b3bd9aa819ced9963f6afa71aa31a9b52699beca
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-22 13:42:09 -0400

    Prairie Mall 2.0 1.0  /promotions.vue

--- promotions.vue
+++ promotions.vue
@@ -141,8 +141,10 @@
             methods: {
                 loadData: async function() {
                     try {
-                        // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
-                        let results = await Promise.all([this.$store.dispatch("getData", "promotions"), this.$store.dispatch("getData", "repos")]);
+                        let results = await Promise.all([
+                            this.$store.dispatch("getData", "promotions"), 
+                            this.$store.dispatch("getData", "repos")
+                        ]);
                         console.log(results)
                     } catch (e) {
                         console.log("Error loading data: " + e.message);
