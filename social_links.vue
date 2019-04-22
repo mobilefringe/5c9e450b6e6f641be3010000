3aa021c65b5efd75a4a37ef41aabb20fe008d887
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-21 21:22:49 -0400

    Prairie Mall 2.0 1.0  /social_links.vue

--- social_links.vue
+++ social_links.vue
@@ -1,20 +1,30 @@
 <template>
     <div>
-        <a href="https://www.facebook.com/Prairiemall" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i><p style="display:none">Facebook</p></a>
-        <a href="https://www.twitter.com/prairiemall" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i><p style="display:none">Twitter</p></a>
-        <a href="https://www.pinterest.ca/prairiemall" target="_blank"><i class="fa fa-pinterest" aria-hidden="true"></i><p style="display:none">Twitter</p></a>
-        <a href="https://www.instagram.com/prairiemall/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i><p style="display:none">Instagram</p></a>
+        <a href="https://www.facebook.com/Prairiemall" target="_blank">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+            <p style="display:none">Facebook</p>
+        </a>
+        <a href="https://www.twitter.com/prairiemall" target="_blank">
+            <i class="fa fa-twitter" aria-hidden="true"></i>
+            <p style="display:none">Twitter</p>
+        </a>
+        <a href="https://www.pinterest.ca/prairiemall" target="_blank">
+            <i class="fa fa-pinterest" aria-hidden="true"></i>
+            <p style="display:none">Twitter</p>
+        </a>
+        <a href="https://www.instagram.com/prairiemall/" target="_blank">
+            <i class="fa fa-instagram" aria-hidden="true"></i>
+            <p style="display:none">Instagram</p>
+        </a>
     </div>
-    
 </template>
 <style>
-    .lb-data .lb-number, .lb-nav {
-        display:none!important;
-    }
+    /*.lb-data .lb-number, .lb-nav {*/
+    /*    display:none!important;*/
+    /*}*/
 </style>
 <script>
-    define(["Vue", "vuex", "lightbox"], function(Vue, Vuex, Lightbox) {
-        Vue.use(Lightbox);
+    define(["Vue", "vuex"], function(Vue, Vuex) {
         return Vue.component("social-links", {
             template: template, // the variable template will be injected,
         });
