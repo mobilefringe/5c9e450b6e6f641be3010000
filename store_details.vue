7574d80594b1d4012e4155ea5a5f150ef5c8ade1
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-22 11:07:46 -0400

    Prairie Mall 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -18,14 +18,16 @@
 			    </div>
 				<div class="col-sm-4 promo_logo_container hidden_phone">
 					<div class="image_container details_store_image">
-						<!--<img v-lazy="currentStore.store_front_url_abs" class="image"/>-->
+
 						<div v-if="currentStore.no_store_logo" class="store_details_image center-block">
                             <div class="no_logo">
                                 <img class="store_img" src="//www.mallmaverick.com/system/site_images/photos/000/041/782/original/transparent_logo.png?1533845225" alt="">
                                 <h2 class="store_details_name">{{ currentStore.name }}</h2>
                             </div>    
                         </div>
-                        <img v-else class="store_details_image center-block" :src="currentStore.store_front_url_abs" :alt="currentStore.name + ' Logo'" />
+                        <div v-else class="store_details_image center-block">
+                            <img :src="currentStore.store_front_url_abs" :alt="currentStore.name + ' Logo'" />
+                        </div>
 					</div>
 				</div>
 				<div class="col-sm-8 promo_image_container text-left">
