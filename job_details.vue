a328df9503308977bdf1789d715cd877e2e77cea
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-21 20:59:02 -0400

    Prairie Mall 2.0 1.0  /job_details.vue

--- job_details.vue
+++ job_details.vue
@@ -18,14 +18,15 @@
 			<div class="row">
 				<div class="col-sm-4 promo_logo_container hidden_phone">
 					<div class="image_container details_store_image">
-						<!--<img v-lazy="currentStore.store_front_url_abs" class="image"/>-->
 						<div v-if="currentJob.store.no_store_logo" class="store_details_image center-block">
                             <div class="no_logo">
                                 <img class="store_img" src="//www.mallmaverick.com/system/site_images/photos/000/041/782/original/transparent_logo.png?1533845225" alt="">
                                 <h2 class="store_details_name">{{ currentJob.store.name }}</h2>
                             </div>    
                         </div>
-                        <img v-else class="store_details_image center-block" :src="currentJob.store.store_front_url_abs" :alt="currentJob.store.name + ' Logo'" />
+                        <div v-else class="store_details_image center-block">
+                            <img :src="currentJob.store.store_front_url_abs" :alt="currentJob.store.name + ' Logo'" />
+                        </div>
 					</div>
 					<div class="text-center" v-if="currentJob.store.name">
 					    <div v-if="currentJob.jobable_type == 'Store'">
