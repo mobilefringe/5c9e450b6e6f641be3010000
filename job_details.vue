e191a2ba9e5d695fc8dc6429f9a00dac7ff1560e
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-21 20:58:19 -0400

    Prairie Mall 2.0 1.0  /job_details.vue

--- job_details.vue
+++ job_details.vue
@@ -10,8 +10,10 @@
 			</div>
 		</div>
 		<div class="site_container">
-		    <div class="col-sm-12 previous_router">
-    		    <router-link to="/jobs"><i class="fa fa-angle-left"></i> {{$t("jobs_page.back_to_jobs")}}</router-link>
+		    <div class="row">
+		        <div class="col-sm-12 previous_router">
+    		        <router-link to="/jobs"><i class="fa fa-angle-left"></i> {{$t("jobs_page.back_to_jobs")}}</router-link>
+    		    </div>
 		    </div>
 			<div class="row">
 				<div class="col-sm-4 promo_logo_container hidden_phone">
