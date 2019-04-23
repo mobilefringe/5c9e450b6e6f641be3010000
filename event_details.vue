126dc0da13446ebb76ed5eff5e9fc5dd67036c72
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-23 17:47:21 -0400

    Prairie Mall 2.0 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -58,7 +58,7 @@
 </template>
 
 <script>
-    define(['Vue', 'vuex', 'moment'], function(Vue, Vuex, moment) {
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment"], function(Vue, Vuex, moment, tz, VueMoment) {
         return Vue.component("event-details-component", {
             template: template, // the variable template will be injected,
             data: function() {
