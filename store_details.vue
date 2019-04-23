db12f98a8eb53e855f0e3143270e841179713a69
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-23 16:48:33 -0400

    Prairie Mall 2.0 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -44,8 +44,8 @@
 						<ul class="store_hours_list">
 							<li v-if="storeHours" v-for="hour in storeHours" class="col-xs-12">
 							    <span class="col-xs-5 text-left">{{ hour.day_of_week | moment("dddd", timezone) }}</span>
-								<span v-if="hour.is_closed" class="col-xs-7 text-left">Closed</span>
-								<span v-else class="col-xs-7 text-left">{{ hour.open_time | moment("h:mm A", timezone) }} - {{ hour.close_time | moment("h:mm A", timezone) }}</span>
+								<span v-if="hour.is_closed" class="col-xs-7 text-right">Closed</span>
+								<span v-else class="col-xs-7 text-right">{{ hour.open_time | moment("h:mm A", timezone) }} - {{ hour.close_time | moment("h:mm A", timezone) }}</span>
 							</li>
 						</ul>
 					</div>
