8a5fe53bc99f84c116d1323959f0356b1aa38028
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-23 17:46:53 -0400

    Prairie Mall 2.0 1.0  /events.vue

--- events.vue
+++ events.vue
@@ -128,7 +128,7 @@
 </style>
 
 <script>
-    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment", "vue-meta", "vue-lazy-load", "vue-paginate"], function(Vue, Vuex, moment, tz, VueMoment, Meta, VueLazyload, VuePaginate) {
+    define(["Vue", "vuex", "moment", "moment-timezone", "vue-moment"], function(Vue, Vuex, moment, tz, VueMoment) {
         Vue.use(Meta);
         Vue.use(VueLazyload);
         Vue.use(VuePaginate);
