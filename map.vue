c3eba557e69539ceacc988ea8449ffe37e7e34b7
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-23 17:43:31 -0400

    Prairie Mall 2.0 1.0  /map.vue

--- map.vue
+++ map.vue
@@ -1,6 +1,6 @@
 <template>
 	<div v-if="dataloaded">
-	    <div class="page_header" v-if="pageBanner" :style="{ backgroundImage: 'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.6) 61.52%, rgba(0,0,0,0.8) 100%), url(' + pageBanner.image_url + ')' }">
+	    <div class="page_header" v-if="pageBanner" :style="{ backgroundImage: 'linear-gradient(180deg, rgba(0,0,0,0.3) 0%, rgba(0,0,0,0.5) 61.52%, rgba(0,0,0,0.7) 100%), url(' + pageBanner.image_url + ')' }">
 			<div class="site_container">
 				<div class="header_content">
 					<h1>{{ $t("stores_page.map") }}</h1>
