09d13dd59aeb9d1e8dbf4fbe4c83a10915864af4
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-04-26 13:28:47 -0400

    Prairie Mall 2.0 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="contact_us_container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
-        <div class="page_header" v-if="pageBanner" :style="{ backgroundImage: 'linear-gradient(180deg, rgba(0,0,0,0.3) 0%, rgba(0,0,0,0.5) 61.52%, rgba(0,0,0,0.7) 100%), url(' + pageBanner.image_url + ')' }">
+		<div class="page_header" v-if="pageBanner" :style="{ backgroundImage: 'linear-gradient(rgba(255, 255, 255, 0.3) 0%, rgba(169, 169, 169, 0.5) 61.52%, rgba(117, 117, 117, 0.7) 100%), url(' + pageBanner.image_url + ')' }">
 			<div class="site_container">
 				<div class="header_content">
 					<h1>{{$t("newsletter_page.newsletter")}}</h1>
