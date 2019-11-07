4333717852735c597078de753e3a6b64991d76a9
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-11-07 11:37:30 -0500

    Prairie Mall 1.0  /default.vue

--- default.vue
+++ default.vue
@@ -4,16 +4,16 @@
 
 <script>
     define(["Vue", "vue-meta"], function(Vue, Meta) {
-        Vue.use(Meta);
         return Vue.component("default-component", {
             template: template, // the variable template will be injected
             props:['locale'],
             data: function() {
                 return {
                     meta: {
-                        meta_title: "",
-                        meta_description: "",
-                        meta_keywords: ""
+                       meta_title: "",
+                       meta_description: "",
+                       meta_keywords: "",
+                       meta_image: ""
                     }
                 }
             },
@@ -33,13 +33,16 @@
                 }
             },
             metaInfo () {
-                return {
-                    title: this.meta.meta_title,
-                    meta: [
-                        { name: 'description', content: this.meta.meta_description },
-                        { name: 'keywords', content: this.meta.meta_keywords }
-                    ]
-                }
+               return {
+                  title: this.meta.meta_title,
+                  meta: [
+                     { name: 'description', vmid: 'description', content: this.meta.meta_description },
+                     { name: 'keywords',  vmid: 'keywords', content: this.meta.meta_keywords },
+                     { property: 'og:title', vmid: 'og:title', content: this.meta.meta_title },
+                     { property: 'og:description', vmid: 'og:description', content: this.meta.meta_description },
+                     { property: 'og:image', vmid: 'og:image', content: this.meta.meta_image }
+                  ]
+               }
             }
         });
     });
